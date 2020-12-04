51f7ec37a060753acb0723d81479611ad1be6e1d
Ab-1908 <69389229+Ab-1908@users.noreply.github.com>
2020-12-04 20:00:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,21 +29,24 @@ function setup() {
 function draw() {  
     background(46,139,87);
 
-  drawSprites();
-  //add styles here
-
-  textSize(15);
-  fill("white");
-  text("NOTE: press UP ARROW KEY to feed drago milk!", 100, 20)
-  
-
-  if(keyWentDown(UP_ARROW))
+    if(keyWentDown(UP_ARROW))
   {
      writeStock(foodS);
      dog.addImage(happydogImage);
     
 
   }
+
+  drawSprites();
+  //add styles here
+
+  textSize(15);
+  fill("white");
+  text("Food remaining" + foodS, 170, 100);
+  text("NOTE: press UP ARROW KEY to feed drago milk!", 100, 20)
+  
+
+  
 }
 
 function readStock(data)
@@ -54,6 +57,15 @@ foodS = data.val();
 
 function writeStock(x)
 {
+  if(x<=0)
+  {
+    x=0;
+  }
+  else
+  {
+    x=x-1
+  }
+
 database.ref('/').update({
    Food:x
 })
